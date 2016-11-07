b4c6ce3304b69161d12eed5131fa4793ff304b1d
Xiaoyun Yu <sxdxyxy@gmail.com>
2016-11-07 21:13:36 +0800

    First commit.

---
+++ plugins/xelatex/xelatex.bat
@@ -0,0 +1,5 @@
+xelatex.exe -interaction nonstopmode %1 %2 %3 %4 %5
+bibtex.exe  %1 %2 %3 %4 %5
+xelatex.exe -interaction nonstopmode %1 %2 %3 %4 %5
+xelatex.exe -interaction nonstopmode %1 %2 %3 %4 %5
+exit 0
